0c2b6c03c84ff73513a2588210b23d2a3517f31c
Fahmida Faiza <fahmidafaiza57@gmail.com>
2024-01-02 21:51:12 +0600

    our brand design modify

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -17,32 +17,32 @@ import 'swiper/css/pagination';
 
 
 const Banner = () => {
-    return (
-        <>
-        <Carousel>
-            <div>
-               <img src={img2} alt="" />
-                
-            </div>
-            <div>
-                <img src={img2} alt="" />
-               
-            </div>
-            <div>
-                <img src={img2} alt="" />
-                
-            </div>
-        </Carousel>
+  return (
+    <>
+      <Carousel>
+        <div>
+          <img src={img2} alt="" />
 
-<br></br>
-<div className="my-30 text-center font-extrabold text-5xl">
-    <p>This our new brand</p>
+        </div>
+        <div>
+          <img src={img2} alt="" />
 
-</div>
+        </div>
+        <div>
+          <img src={img2} alt="" />
 
- <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
+        </div>
+      </Carousel>
+
+      <br></br>
+      <div className="my-30 text-center font-extrabold text-5xl">
+        <p>This our new brand</p>
+
+      </div>
+
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={20}
         pagination={{
           clickable: true,
         }}
@@ -50,95 +50,131 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-                    <img src={slide2} alt="" />
+          <div className="card w-72 bg-slate-100 shadow-xl">
+            <figure><img src={slide2} alt="Shoes" /></figure>
+            <div className="card-body text-center">
+              <h2 className="text-xl font-medium">Nail Polish</h2>
+              <p className="text-xl font-medium">$200</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-                    <img src={slide2} alt="" />
+           <div className="card w-72 bg-slate-100 shadow-xl">
+            <figure><img src={slide2} alt="Shoes" /></figure>
+            <div className="card-body text-center">
+              <h2 className="text-xl font-medium">Nail Polish</h2>
+              <p className="text-xl font-medium">$200</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-                    <img src={slide2} alt="" />
+           <div className="card w-72 bg-slate-100 shadow-xl">
+            <figure><img src={slide2} alt="Shoes" /></figure>
+            <div className="card-body text-center">
+              <h2 className="text-xl font-medium">Nail Polish</h2>
+              <p className="text-xl font-medium">$200</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-                    <img src={slide2} alt="" />
+           <div className="card w-72 bg-slate-100 shadow-xl">
+            <figure><img src={slide2} alt="Shoes" /></figure>
+            <div className="card-body text-center">
+              <h2 className="text-xl font-medium">Nail Polish</h2>
+              <p className="text-xl font-medium">$200</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-                    <img src={slide2} alt="" />
+           <div className="card w-72 bg-slate-100 shadow-xl">
+            <figure><img src={slide2} alt="Shoes" /></figure>
+            <div className="card-body text-center">
+              <h2 className="text-xl font-medium">Nail Polish</h2>
+              <p className="text-xl font-medium">$200</p>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-                    <img src={slide2} alt="" />
+           <div className="card w-72 bg-slate-100 shadow-xl">
+            <figure><img src={slide2} alt="Shoes" /></figure>
+            <div className="card-body text-center">
+              <h2 className="text-xl font-medium">Nail Polish</h2>
+              <p className="text-xl font-medium">$200</p>
+            </div>
+          </div>
         </SwiperSlide>
-      
+
       </Swiper>
 
-        <div>
+      <div>
 
-          <div className="hero min-h-screen ">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-              <img src={img3} className="w-72 lg:w-96 rounded-lg shadow-2xl" />
-              <div>
-                <h1 className="text-5xl font-bold">EXPRESS YOURSELF <br></br>THROUGH NIOR<br></br> LIPSTICKS</h1>
-                <p className="py-6">When you can wear a statement, why make one?<br></br> Pick a matte lipstick that provides excellent coverage and transfer-proof color in one stroke.<br></br> The only thing you leave behind is your vibe, since the color stays with you the entire day!
-                  <br></br>
+        <div className="hero min-h-screen ">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img src={img3} className="w-72 lg:w-96 rounded-lg shadow-2xl" />
+            <div>
+              <h1 className="text-5xl font-bold">EXPRESS YOURSELF <br></br>THROUGH NIOR<br></br> LIPSTICKS</h1>
+              <p className="py-6">When you can wear a statement, why make one?<br></br> Pick a matte lipstick that provides excellent coverage and transfer-proof color in one stroke.<br></br> The only thing you leave behind is your vibe, since the color stays with you the entire day!
+                <br></br>
 
 
-                  A Tale of Enduring Influence on the Present and the Future</p>
-                <button className="btn btn-outline btn-secondary">Shop Now</button>
-              </div>
+                A Tale of Enduring Influence on the Present and the Future</p>
+              <button className="btn btn-outline btn-secondary">Shop Now</button>
             </div>
           </div>
         </div>
-
-
-      <div>
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={img2} alt="" />
-            </SwiperSlide>
-          </Swiper>
       </div>
 
 
-    
+      <div>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img2} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
 
-      </>
-    );
+
+
+
+    </>
+  );
 };
 
 export default Banner;
